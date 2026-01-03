e4b39c5d02ec2f58e315c9b36087a11c92fc5efb
Moon-12 <ashwijanayak@gmail.com>
2026-01-02 20:00:21 -0500

    reverse string implemented

--- ReverseString/src/Main.java
+++ ReverseString/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        String str="HelloWorld";
+        System.out.println(reverse(str));
+    }
+    public static String reverse(String str){
+        StringBuilder reversedStr=new StringBuilder();
+       char[] charArr= str.toCharArray();
+       for(int i=charArr.length-1;i>=0;i--)
+       {
+           reversedStr.append(charArr[i]);
+       }
+        return reversedStr.toString();
     }
 }
